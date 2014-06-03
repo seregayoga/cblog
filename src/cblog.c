708970b76cc63222cb8cfb4a0c58b8e59c64d286
Sergey Fedchenko <seregayoga@bk.ru>
2014-06-03 23:54:00 +0700

    fix english grammar

--- src/cblog.c
+++ src/cblog.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     int offset = 0;
     /* TODO remove hardcore */
     char *formatted_query = "SELECT * FROM posts LIMIT %d OFFSET %d";
-    /* plus size of two digits and the end of string */
+    /* plus size of two digits and the end of the string */
     char *query = (char *)malloc(sizeof(char) * (sizeof(formatted_query) + 2 * 10 + 1));
 
     
